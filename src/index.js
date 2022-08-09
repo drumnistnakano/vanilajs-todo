cf605ddd14565fd4ae8fbb5bac728b459c14cca2
Yoshiyuki Nakano <syxqh349ysykn@gmail.com>
2022-08-10 00:39:40 +0900

    Add: 完了・削除ボタンの追加

--- src/index.js
+++ src/index.js
@@ -13,9 +13,18 @@ const onClickAdd = () => {
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  // button 完了タグ生成
+  const complateButton = document.createElement("button");
+  complateButton.innerText = "完了";
+
+  // button 削除タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+
   // div タグの子要素に各要素を設定
   div.appendChild(li);
-  console.log(div);
+  div.appendChild(complateButton);
+  div.appendChild(deleteButton);
 
   //　未完了のリストに追加
   document.getElementById("incomplete-list").appendChild(div);
